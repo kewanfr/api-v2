711c50a9ee16d053ffad0db7ca1def85127bdd10
Kéwan Bernier <mail@kewan.fr>
2024-04-07 20:34:40 +0200

    Mise à jour du modèle User.js avec des modifications de structure et de définition des attributs.

--- models/User.js
+++ models/User.js
@@ -1,33 +1,41 @@
 import Sequelize from 'sequelize';
 import sequelize from './database.js';
 
-const User = sequelize.define("user", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true,
-  },
-  username: {
-    type: Sequelize.STRING,
-    unique: true,
-    allowNull: false,
-  },
-  email: {
-    type: Sequelize.STRING,
-    unique: true,
-    allowNull: false,
-  },
+const User = sequelize.define(
+  "user",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: {
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: false,
+    },
 
-  password: {
-    type: Sequelize.STRING,
-    allowNull: true,
+    password: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    role: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue: "user",
+    },
   },
-  role: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    defaultValue: "user",
-  },
-});
+  {
+    defaultScope: {
+      attributes: { exclude: ["password"] },
+    },
+  }
+);
 
 export default User;
