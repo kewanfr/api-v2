e7c9402581d90ba1a67b0cd3b0e19214f48f30e9
Kéwan Bernier <mail@kewan.fr>
2024-04-07 20:06:18 +0200

    Mise à jour du modèle User pour inclure une clé primaire et des contraintes de non-nullité et d'unicité pour les champs username et email.

--- models/User.js
+++ models/User.js
@@ -1,29 +1,33 @@
 import Sequelize from 'sequelize';
 import sequelize from './database.js';
 
-const User = sequelize.define('user', {
+const User = sequelize.define("user", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
   username: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
   },
   email: {
     type: Sequelize.STRING,
     unique: true,
-    allowNull: false
+    allowNull: false,
   },
 
   password: {
     type: Sequelize.STRING,
-    allowNull: true
+    allowNull: true,
   },
   role: {
     type: Sequelize.STRING,
     allowNull: false,
-    defaultValue: 'user'
+    defaultValue: "user",
   },
-  
-
 });
 
 export default User;
