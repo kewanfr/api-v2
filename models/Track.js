56581378aaa6535a371de4368fa27f4ec6725483
Kéwan Bernier <mail@kewan.fr>
2024-04-26 00:42:26 +0200

    track db model

--- models/Track.js
+++ models/Track.js
@@ -10,36 +10,54 @@ const Track = sequelize.define(
       allowNull: false,
       primaryKey: true,
     },
-    title: {
+    // Can be spotify or youtube
+    // origin: {
+      
+    // }
+    name: {
       type: Sequelize.STRING,
       allowNull: false,
     },
     artists: {
-      type: Sequelize.STRING,
-      allowNull: false,
-    },
-    album_artist: {
-      type: Sequelize.STRING,
+      type: Sequelize.ARRAY(Sequelize.STRING),
       allowNull: true,
     },
     album_name: {
       type: Sequelize.STRING,
       allowNull: true,
     },
-    cover: {
+    release_date: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    cover_url: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    track_number: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+
+    spotify_id: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    spotify_url: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+    youtube_url: {
+      type: Sequelize.STRING,
+      allowNull: true,
+    },
+
+    status: {
       type: Sequelize.STRING,
       allowNull: true,
     },
     path: {
       type: Sequelize.STRING,
-      allowNull: false,
-    },
-    track_position: {
-      type: Sequelize.INTEGER,
-      allowNull: true,
-    },
-    lyrics: {
-      type: Sequelize.BOOLEAN,
       allowNull: true,
     },
   }
