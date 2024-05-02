5a9d75c6968287a405eb9e545d4b3c9f25b752ab
Kéwan Bernier <mail@kewan.fr>
2024-05-02 22:35:10 +0200

    feat: ✨MusicFunctions

---
+++ utils/music/music.js
@@ -0,0 +1,409 @@
+import SpotifyGet from "spotify-get";
+import config from "../../config.js";
+
+import Download_Queue from "../../models/Download_Queue.js";
+import Track from "../../models/Track.js";
+
+import fs from "fs";
+import {
+  ensureDir,
+  parseAlbumResult,
+  parseArtistResult,
+  parseTrackResult,
+} from "../functions.js";
+import { Spotify } from "spotifydl-core";
+import { Op } from "sequelize";
+import path from "path";
+
+import * as YTMusic from "node-youtube-music";
+
+export class MusicFunctions {
+  constructor() {
+    this.spotifyClient = new SpotifyGet({
+      consumer: {
+        key: config.music.spotify_client_id,
+        secret: config.music.spotify_client_secret,
+      },
+    });
+
+    this.spotifyDownloader = new Spotify(
+      {
+        clientId: config.music.spotify_client_id,
+        clientSecret: config.music.spotify_client_secret,
+      },
+      config.music.youtube_cookie
+    );
+
+    this.TEMP_SONGS_PATH = config.music.paths.temp_songs;
+    this.TEMP_COVERS_PATH = config.music.paths.temp_covers;
+    this.FINAL_PATH = config.music.paths.final;
+
+    if (!fs.existsSync(this.TEMP_SONGS_PATH)) {
+      fs.mkdirSync(this.TEMP_SONGS_PATH, { recursive: true });
+    }
+
+    if (!fs.existsSync(this.TEMP_COVERS_PATH)) {
+      fs.mkdirSync(this.TEMP_COVERS_PATH, { recursive: true });
+    }
+
+    if (!fs.existsSync(this.FINAL_PATH)) {
+      fs.mkdirSync(this.FINAL_PATH, { recursive: true });
+    }
+
+    this.IS_QUEUE_DOWNLOADING = false;
+
+    this.downloadQueue();
+
+    this.socket = null;
+  }
+
+  setSocket(socket) {
+    this.socket = socket;
+  }
+
+  getSocket() {
+    return this.socket;
+  }
+
+  sendSocketMessage(data) {
+    if (this.socket) {
+      this.socket.send(JSON.stringify(data));
+    }
+  }
+
+  async search(query, type = "track", limit = 20) {
+    type = type.replace(/ /g, "");
+    // Search for a song on Spotify and return clean results
+    if (type == "*") {
+      type = "artist,album,track";
+    }
+
+    const searchDatas = await this.spotifyClient.search({
+      q: `${query}`,
+      type: type,
+      limit: limit,
+    });
+
+    const results = {};
+
+    if (searchDatas.tracks?.items) {
+      results.tracks = await Promise.all(
+        searchDatas.tracks.items.map(async (item) => {
+          return await parseTrackResult(item, { searchYoutube: false });
+        })
+      );
+    }
+
+    if (searchDatas.albums?.items) {
+      results.albums = await Promise.all(
+        searchDatas.albums.items.map((item) => parseAlbumResult(item))
+      );
+    }
+
+    if (searchDatas.artists?.items) {
+      results.artists = await Promise.all(
+        searchDatas.artists.items.map((item) => parseArtistResult(item))
+      );
+    }
+
+    return results;
+  }
+
+  async searchYoutube(query) {
+    const musics = await YTMusic.searchMusics(query);
+
+    const results = {
+      tracks: [],
+      albums: [],
+      artists: [],
+    };
+
+    results.tracks = musics.map((item) => {
+      return {
+        name: item.title,
+        artist: item.artists[0]?.name,
+        artists: item.artists?.map((artist) => artist.name),
+        album_name: item.album,
+        youtube_id: item.youtubeId,
+        cover_url: item.thumbnailUrl?.split("=")[0] ?? item.thumbnailUrl,
+        duration_ms: item.duration.totalSeconds * 1000,
+      };
+    });
+
+    return results;
+  }
+
+  async getArtistTracks(artist_id) {
+    const token = await this.spotifyClient.getToken();
+    const API_URL = `https://api.spotify.com/v1/artists/${artist_id}/top-tracks?market=FR`;
+
+    const response = await fetch(API_URL, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await response.json();
+
+    return await Promise.all(
+      data.tracks.map(async (item) => {
+        return await parseTrackResult(item, { searchYoutube: false });
+      })
+    );
+  }
+
+  async getTrack(track_id) {
+    const track_data = await this.spotifyDownloader.getTrack(track_id);
+    console.log(track_data, "track_data");
+    return track_data;
+  }
+
+  async getDownloadQueue() {
+    const results = await Download_Queue.findAll({
+      where: {
+        status: config.QUEUE_STATUS.PENDING,
+      },
+    });
+
+    const queue = results.map((item) => item.dataValues);
+    // this.sendSocketMessage({
+    //   action: "queue",
+    //   queue: queue,
+    // });
+
+    return queue;
+  }
+
+  async searchQueueByName(name) {
+    const results = await Download_Queue.findOne({
+      where: {
+        name: {
+          [Op.like]: "%" + name + "%",
+        },
+      },
+    });
+
+    return results.dataValues;
+  }
+
+  async searchQueueById(id) {
+    const results = await Download_Queue.findOne({
+      where: {
+        id: id,
+      },
+    });
+
+    return results.dataValues;
+  }
+
+  async searchQueueBySpotifyId(spotify_id) {
+    const results = await Download_Queue.findOne({
+      where: {
+        spotify_id: spotify_id,
+      },
+    });
+
+    return results.dataValues;
+  }
+
+  async addSongToQueue(track_data, user_id = null) {
+    track_data.artists = track_data.artists.join(", ");
+
+    const search_track_queue = await Download_Queue.findOne({
+      where: {
+        spotify_id: track_data.spotify_id,
+      },
+    });
+
+    const search_track = await Track.findOne({
+      where: {
+        spotify_id: track_data.spotify_id,
+      },
+    });
+
+    if (search_track || search_track_queue) {
+      console.log(
+        `Track ${track_data.name} - ${track_data.artists} already in queue`
+      );
+      return config.QUEUE_STATUS.ALREADY_IN_QUEUE;
+    }
+
+    track_data.user_id = user_id;
+    await Download_Queue.create(track_data);
+
+    console.log(
+      `Track ${track_data.name} - ${track_data.artists} added to queue`
+    );
+
+    this.sendSocketMessage({
+      action: "song_added_to_queue",
+      song: track_data,
+      queue: await this.getDownloadQueue(),
+    });
+
+    this.downloadQueue();
+    return config.QUEUE_STATUS.PENDING;
+  }
+
+  async moveSongToFinalPath(file_name, track_data) {
+    const folder_name = path.join(
+      track_data.artists[0],
+      track_data.album_name
+    );
+
+    const final_path = path.join(this.FINAL_PATH, folder_name, file_name);
+    const temp_path = path.join(this.TEMP_SONGS_PATH, file_name);
+
+    await ensureDir(path.join(this.FINAL_PATH, folder_name));
+
+    if (!fs.existsSync(temp_path)) {
+      console.error(`File ${file_name} not found in temp path`);
+      return;
+    }
+
+    this.sendSocketMessage({
+      action: "song_downloaded",
+      song: track_data,
+      queue: await this.getDownloadQueue(),
+    });
+
+    await fs.renameSync(temp_path, final_path);
+  }
+
+  async downloadQueue() {
+    if (this.IS_QUEUE_DOWNLOADING) return;
+
+    const download_queue = await this.getDownloadQueue();
+
+    if (download_queue.length === 0) {
+      console.log("Queue is empty");
+      return;
+    }
+
+    this.IS_QUEUE_DOWNLOADING = true;
+
+    const track_info = download_queue[0];
+
+    await Download_Queue.update(
+      { status: config.QUEUE_STATUS.DOWNLOADING },
+      {
+        where: {
+          id: track_info.id,
+        },
+      }
+    );
+
+    this.sendSocketMessage({
+      action: "song_downloading",
+      song: track_info,
+      queue: await this.getDownloadQueue(),
+    });
+
+    console.log(`Downloading ${track_info.name} - ${track_info.artists}`);
+
+    const filePath = await path.join(
+      this.TEMP_SONGS_PATH,
+      `${track_info.name} - ${track_info.artists}.mp3`
+    );
+
+    track_info.artists = track_info.artists.split(", ");
+
+    try {
+      const result = await this.spotifyDownloader.downloadTrackFromInfo(
+        track_info,
+        filePath
+      );
+
+      if (result.error) {
+        throw new Error(`Error while downloading ${track_info.name}`);
+      }
+
+      await this.moveSongToFinalPath(
+        `${track_info.name} - ${track_info.artists}.mp3`,
+        track_info
+      );
+
+      await Download_Queue.update(
+        { status: config.QUEUE_STATUS.FINISHED },
+        {
+          where: {
+            id: track_info.id,
+          },
+        }
+      );
+
+      track_info.artists = track_info.artists.join(", ");
+
+      await Track.create({
+        ...track_info,
+        path: await path.join(
+          this.FINAL_PATH,
+          `${track_info.name} - ${track_info.artists}.mp3`
+        ),
+      });
+
+      console.log(`Sucessfully downloaded ${track_info.name}\n`);
+    } catch (error) {
+      console.error(
+        `Error while downloading ${
+          track_info.name
+        } - ${track_info.artists.join(", ")}`,
+        error
+      );
+
+      await Download_Queue.update(
+        { status: config.QUEUE_STATUS.ERROR },
+        {
+          where: {
+            id: track_info.id,
+          },
+        }
+      );
+
+      return {
+        error: true,
+        track: track_info.name + " - " + track_info.artist,
+      };
+    }
+
+    this.IS_QUEUE_DOWNLOADING = false;
+    this.downloadQueue();
+  }
+
+  async downloadFromDatas(track_data, user_id = null) {
+    return await this.addSongToQueue(track_data, user_id);
+  }
+
+  async downloadFromSpotifyId(spotify_id, user_id = null) {
+    console.log(spotify_id, "spotify_id");
+    // const token = await this.spotifyClient.getToken();
+    // const API_URL = `https://api.spotify.com/v1/tracks/${track_id}?market=FR`;
+
+    // const response = await fetch(API_URL, {
+    //   method: "GET",
+    //   headers: {
+    //     Authorization: `Bearer ${token}`,
+    //   },
+    // });
+
+    // const item = await response.json();
+
+    // let track_data = await parseTrackResult(item, { searchYoutube: true });
+
+    let track_data = await this.spotifyDownloader.getTrack(spotify_id);
+
+    return await this.addSongToQueue(track_data, user_id);
+
+    try {
+      const result = await this.downloadFromDatas(track_data);
+      return result;
+    } catch (error) {
+      console.error("Error while downloading", error);
+      return {
+        error: true,
+        track: track_data.name + " - " + track_data.artist,
+      };
+    }
+  }
+}
