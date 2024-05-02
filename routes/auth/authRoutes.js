f79e7f1436519a16fd67d8c0678dd6e9b51dcf10
Kéwan Bernier <mail@kewan.fr>
2024-05-02 22:33:16 +0200

    feat: ✨ /auth/verifyToken route that verify if logged and return token data

--- routes/auth/authRoutes.js
+++ routes/auth/authRoutes.js
@@ -6,7 +6,7 @@ import {
 } from "./authController.js";
 
 import verifyLogged from "../../utils/verifyLogged.js";
-
+import verifyToken from "../../utils/verifyToken.js";
 
 const loggedAuthRoutes = (fastify) => {
   fastify.get("/auth/me", {
@@ -24,6 +24,23 @@ export default (fastify, options, done) => {
     handler: authLoginHandler,
   });
 
+  fastify.get("/auth/verifyToken", {
+    handler: async (req, reply) => {
+      const token = req.headers.Authorization || req.headers.authorization;
+
+      if (!token) {
+        return reply.code(400).send({ message: "Missing token" });
+      }
+
+      const data = await verifyToken(token);
+
+      if (!data) {
+        return reply.code(401).send({ message: "Invalid token" });
+      }
+
+      return reply.send(data);
+    },
+  });
 
   loggedAuthRoutes(fastify);
 
