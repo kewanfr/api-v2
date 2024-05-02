b6ea2f558d32fcdb0cfdb3fe3a97b066912c9acc
Kéwan Bernier <mail@kewan.fr>
2024-05-02 22:31:55 +0200

    feat: ✨Send Infos route

--- routes/index/indexController.js
+++ routes/index/indexController.js
@@ -1,6 +1,36 @@
 
+import packagejson from "../../package.json" assert { type: "json" };
+
+import os from "os";
+import isAdmin from "../../utils/isAdmin.js";
 
 export const index = async (req, reply) => {
+  return { message: "Hello World" };
+};
 
-    return { message: 'Hello World' };
+export const infos = async (req, reply) => {
+  if ((await isAdmin(req)) == true) {
+    return {
+      message: "Infos",
+      version: packagejson.version,
+      fastifyVersion: packagejson.dependencies.fastify,
+      nodeVersion: process.version,
+      platform: process.platform,
+      uptime: process.uptime(),
+      memoryUsage: process.memoryUsage(),
+      cpus: os.cpus(),
+      networkInterfaces: os.networkInterfaces(),
+      userInfo: os.userInfo(),
+    };
+  } else {
+    reply.code(401);
+
+    reply.send({
+      statusCode: 401,
+      message: "You are not an admin",
+      version: packagejson.version,
+      platform: process.platform,
+      nodeVersion: process.version,
+    });
+  }
 };
