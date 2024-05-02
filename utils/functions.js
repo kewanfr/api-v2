94c058d24fa2c645f5aa8402df8277d0c0033f35
Kéwan Bernier <mail@kewan.fr>
2024-05-02 22:31:16 +0200

    refactor: 📦getDirectories

--- utils/functions.js
+++ utils/functions.js
@@ -1,9 +1,12 @@
-import fs from 'fs';
+import fs from "fs";
+import packageJson from "../package.json" assert { type: "json" };
+
 export const getDirectories = async (source) => {
-  return fs.readdirSync(source, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name);
-}
+  return fs
+    .readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+};
 
 export const getFiles = async (source) => {
   return fs
