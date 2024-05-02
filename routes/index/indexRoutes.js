0de34203a47f313dff326b48d6dfcf6953b8588b
Kéwan Bernier <mail@kewan.fr>
2024-05-02 22:32:31 +0200

    refactor: 📦index routes only infos

--- routes/index/indexRoutes.js
+++ routes/index/indexRoutes.js
@@ -1,7 +1,7 @@
-import { index } from "./indexController.js";
+import { index, infos } from "./indexController.js";
 
 export default (fastify, options, done) => {
-  fastify.all('/', index);
+  fastify.get("/infos", infos);
 
   done();
-} 
+};
